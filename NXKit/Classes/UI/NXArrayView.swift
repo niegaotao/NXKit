f4346f4748751ee7033a8c501930b618454603c4
niegaotao <niegaotao@163.com>
2021-08-29 10:10:16 +0800

    0.7.7

--- NXKit/Classes/UI/NXArrayView.swift
+++ NXKit/Classes/UI/NXArrayView.swift
@@ -7,15 +7,16 @@
 
 import UIKit
 
-open class NXArrayView<C:UIView>: NXView {
-    open var contentViews = [C]()
+open class NXArrayView<S:UIView>: NXView {
+    open var contentViews = [S]()
     
     //子类手动调用
     open func setupSubviews(_ numberOfSubviews:Int) {
         var __numberOfSubviews = numberOfSubviews - self.contentViews.count;
         while __numberOfSubviews >= 1 {
-            let contentView = C(frame: CGRect.zero)
+            let contentView = S(frame: CGRect.zero)
             self.contentViews.append(contentView)
+            self.addSubview(contentView)
             __numberOfSubviews = __numberOfSubviews - 1
         }
     }
