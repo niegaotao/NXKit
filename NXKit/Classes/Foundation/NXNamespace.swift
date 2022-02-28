a8817c8bd69c959620bcbf406fa9321ec68b9068
niegaotao <niegaotao@163.com>
2022-02-28 11:45:36 +0800

    -

---
+++ NXKit/Classes/Foundation/NXNamespace.swift
@@ -0,0 +1,61 @@
+//
+//  NXNamespace.swift
+//  NXKit
+//
+//  Created by niegaotao on 2020/1/23.
+//  Copyright © 2020年 TIMESCAPE. All rights reserved.
+//
+
+public struct NXNamespace<RawValue> {
+    public let rawValue : RawValue
+    public init(_ rawValue:RawValue){
+        self.rawValue = rawValue
+    }
+}
+
+public protocol NXNamespaceReferenceWrappable : AnyObject {}
+    
+extension NXNamespaceReferenceWrappable {
+    public var nx : NXNamespace<Self> {
+        set { }
+        get { return NXNamespace<Self>(self) }
+    }
+    
+    static public var nx : NXNamespace<Self>.Type {
+        set { }
+        get { return NXNamespace<Self>.self }
+    }
+}
+
+public protocol NXNamespaceValueWrappable {}
+    
+extension NXNamespaceValueWrappable {
+    public var nx : NXNamespace<Self> {
+        set { }
+        get { return NXNamespace<Self>(self) }
+    }
+    
+    static public var nx : NXNamespace<Self>.Type {
+        set { }
+        get { return NXNamespace<Self>.self }
+    }
+}
+
+/*
+/*1.让类遵守协议*/
+extension UIView : NXNamespaceReferenceWrappable {}
+ 
+/*2.扩展NXNamespace*/
+extension NXNamespace where RawValue : UIView {
+    public func debug(){
+        print("NXNamespace-UIView:frame=\(self.rawValue.frame)");
+    }
+}
+/*使用*/
+func test(){
+    let v = UIView()
+    v.nx.debug()
+    v.nx.debug()
+}
+*/
+
